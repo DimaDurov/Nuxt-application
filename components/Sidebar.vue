efe76374173ec91c28346c9074a5901583e34423
DimaDurov <dima.stepok@yandex.ru>
2020-12-12 20:26:18 +0300

    FinalVersion

---
+++ components/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="left-bar">
+    <div class="left-content">
+      <ul class="action-list">
+        <li class="item">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
+               stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-inbox"
+               viewBox="0 0 24 24">
+            <path d="M22 12h-6l-2 3h-4l-2-3H2" />
+            <path
+              d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z" />
+          </svg>
+          <span>Опросы</span>
+        </li>
+        <li class="item action">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               class="feather feather-users">
+            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+            <circle cx="9" cy="7" r="4" />
+            <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
+            <path d="M16 3.13a4 4 0 0 1 0 7.75" /></svg>
+          <span>Пользователи</span>
+        </li>
+        <li class="item">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor"
+               stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-trash"
+               viewBox="0 0 24 24">
+            <path d="M3 6h18m-2 0v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+          </svg>
+          <span>Черные списки</span>
+        </li>
+        <li class="item">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+               class="feather feather-hash">
+            <line x1="4" y1="9" x2="20" y2="9" />
+            <line x1="4" y1="15" x2="20" y2="15" />
+            <line x1="10" y1="3" x2="8" y2="21" />
+            <line x1="16" y1="3" x2="14" y2="21" /></svg>
+          <span>Колл-центр</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped>
+
+.action {
+ color: #7dd21f;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="page-content">
+      <div class="header">
+        <input class="serach_header" type="text" placeholder="Поиск по системе">
+        <div class="avtor">
+          <h4>Dmitry</h4>
+          <p>Администратор</p>
+        </div>
+      </div>
+      <div class="content-categories">
+        <div class="label-wrapper">
+          <input class="nav-item" name="nav" type="radio" id="opt-1">
+          <label class="category" for="opt-1">Парамерты</label>
+        </div>
+        <div class="label-wrapper">
+          <input class="nav-item" name="nav" type="radio" id="opt-1">
+          <label class="category" for="opt-1">Вопросы</label>
+        </div>
+        <div class="label-wrapper">
+          <input class="nav-item" name="nav" type="radio" id="opt-2">
+          <label class="category" for="opt-2">Логика</label>
+        </div>
+        <div class="label-wrapper">
+          <input class="nav-item" name="nav" type="radio" id="opt-3">
+          <label class="category" for="opt-3">Условия</label>
+        </div>
+        <div class="label-wrapper">
+          <input class="nav-item" name="nav" type="radio" id="opt-4" checked>
+          <label class="category" for="opt-4">Респонденты</label>
+        </div>
+
+      </div>
+      <div class="tasks-wrapper">
+        <div class="task">
+            <span class="label-text">Условие 1</span>
+          <select class="input-task">
+            <option selected disabled>Выберите респодента</option>
+            <option value="Респодент 1">Респодент 1</option>
+            <option value="Респодент 2">Респодент 2</option>
+          </select>
+        </div>
+        <div class="task">
+            <span class="label-text">Дипазон 1</span>
+          <div class="mini-input-task">
+            от <input type="text">
+            до <input type="text">
+          </div>
+        </div>
+        <div class="task" v-if="show">
+          <span class="label-text">Дипазон 1</span>
+          <div class="mini-input-task">
+            от <input type="text">
+            до <input type="text">
+          </div>
+        </div>
+        <div class="button">
+          <button class="add" v-on:click="show = !show">Добавить диапазон</button>
+          <button class="delete" v-on:click="show = !show">Удалить диапазон</button>
+        </div>
+        <div class="header upcoming"></div>
+        <div class="task">
+          <span class="label-text"><b>Условие 2</b></span>
+          <select class="input-task">
+            <option selected disabled>Тип карты лояльности</option>
+            <option value="Респодент 1">Карта 1</option>
+            <option value="Респодент 2">Карта 2</option>
+          </select>
+        </div>
+        <div class="task">
+          <span class="label-text">тип</span>
+          <select class="input-task">
+            <option selected disabled>gold</option>
+            <option value="Респодент 1">siler</option>
+          </select>
+        </div>
+        <div class="task" v-if="showType">
+          <span class="label-text">тип 2</span>
+          <select class="input-task">
+            <option selected disabled>gold</option>
+            <option value="Респодент 1">siler</option>
+          </select>
+        </div>
+        <div class="button">
+          <button class="add" v-on:click="showType = !showType">Добавить диапазон</button>
+          <button class="delete" v-on:click="showType = !showType">Удалить диапазон</button>
+        </div>
+        <div class="header upcoming"></div>
+        <div class="task">
+          <span class="label-text"><b>Условие 3</b></span>
+          <select class="input-task">
+            <option selected disabled>Статус карты лояльности</option>
+            <option value="Респодент 1">Статус 1</option>
+            <option value="Респодент 2">Статус 2</option>
+          </select>
+        </div>
+        <div class="task">
+          <span class="label-text">статус 1</span>
+          <select class="input-task">
+            <option selected disabled>Активна</option>
+            <option value="Респодент 1">Активна</option>
+          </select>
+        </div>
+        <div class="task" v-if="showStatus">
+          <span class="label-text">статус 2</span>
+          <select class="input-task">
+            <option selected disabled>Активна</option>
+            <option value="Респодент 1">Активна</option>
+          </select>
+        </div>
+        <div class="button">
+          <button class="add" v-on:click="showStatus = !showStatus">Добавить диапазон</button>
+          <button class="delete" v-on:click="showStatus = !showStatus">Удалить диапазон</button>
+        </div>
+        <div class="button-footer">
+          <button class="testPool">Протестировать опрос</button>
+        <button class="next">Далее</button>
+        </div>
+      </div>
+      </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      showType: false,
+      showStatus: false,
+    }
+  }
+  }
+</script>
+
+<style scoped>
+.input-task {
+  width: 80%;
+  height: 2em;
+  border: 1px solid #d1d1d1;
+  position: relative;
+
+}
+.mini-input-task {
+  display: inline-block;
+  position: relative;
+  left: -31rem;
+}
+.mini-input-task input {
+  width: 8em;
+  height: 2.5em;
+}
+.delete {
+  float: right;
+  height: 2.5rem;
+  border: 1px solid #fe4d46;
+  color: #fe4d46;
+  background: none;
+}
+.add {
+  position: relative;
+  left: 15rem;
+  height: 2.5rem;
+  border: 1px solid #7dd21f;
+  color: #7dd21f;
+  background: none;
+}
+.testPool {
+  float: left;
+  margin-top: 15px;
+  height: 2.5rem;
+  width: 155px;
+  border: 1px solid #ffffff;
+  border-radius: 5px;
+  color: #00b003;
+  background: #ffffff;
+  box-shadow: 0 0 5px rgba(0,0,0,0.3);
+}
+.next {
+  float: right;
+  margin-top: 15px;
+  height: 2.5rem;
+  width: 95px;
+  border: 1px solid #00b003;
+  border-radius: 5px;
+  color: #ffffff;
+  background: #00b003;
+}
+</style>
